ca14f65a9789e2449efcad17a4b2412b48c892df
Zsolt Miskolczi <zsolt.miskolczi@gmail.com>
2022-06-05 18:23:47 +0200

    Add users table

---
+++ db/migrations/20220523205653_migrations.up.sql
@@ -0,0 +1,18 @@
+create table Users (
+    UserId bigint generated always as identity not null,
+    EmailAddress varchar(255) not null,
+    Activated bit not null,
+    RecordedAt timestamp not null, 
+    UpdatedAt timestamp not null,
+    primary key (UserId)
+);
+
+create index IDX_Users_EmailAddress on Users (EmailAddress);
+
+create table PasswordHashes (
+    UserId bigint not null,
+    PasswordHash varchar(100) not null,
+    RecordedAt timestamp not null,
+    UpdatedAt timestamp not null,
+    primary key (PasswordHash)
+);
